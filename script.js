2d17e7e4ea13adc8ab61fb006de62989e217d49d
tyoprataama <tyoprataama004@gmail.com>
2022-12-12 18:55:45 +0700

    create literal types

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var combine = function (input1, input2, numConvert) {
+var combines = function (input1, input2, numConvert) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || numConvert === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
-    if (numConvert === 'as-number') {
-        return +result; // To convert result as number
-    }
-    else {
-        return result.toString();
-    }
+    return result;
+    // if(numConvert === 'as-number') {
+    //     return +result; // To convert result as number
+    // } else {
+    //     return result.toString();
+    // }
 };
-var combineAges = combine('Max', 45, 'as-text');
-var combineNumber = combine('99', '32', 'as-number');
-console.log(combineAges);
-console.log(combineNumber);
+var combinesAges = combines('Max', 45, 'as-text');
+var combinesNumber = combines('99', '32', 'as-number');
+console.log(combinesAges);
+console.log(combinesNumber);
--- script.ts
+++ script.ts
@@ -1,22 +1,23 @@
-const combine = function(
+const combines = function(
     input1: number | string, 
     input2: number | string,
     numConvert: 'as-number' | 'as-text',
     ) {
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || numConvert === 'as-number') {
+        result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
     }
-    if(numConvert === 'as-number') {
-        return +result; // To convert result as number
-    } else {
-        return result.toString();
-    }
+    return result;
+    // if(numConvert === 'as-number') {
+    //     return +result; // To convert result as number
+    // } else {
+    //     return result.toString();
+    // }
 }
-const combineAges = combine('Max', 45, 'as-text');
-const combineNumber = combine('99', '32', 'as-number')
-console.log(combineAges);
-console.log(combineNumber);
+const combinesAges = combines('Max', 45, 'as-text');
+const combinesNumber = combines('99', '32', 'as-number')
+console.log(combinesAges);
+console.log(combinesNumber);
 
