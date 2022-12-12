40b9b9905a4ed510c27bd8c0b330173b5fe381e4
tyoprataama <tyoprataama004@gmail.com>
2022-12-12 18:48:13 +0700

    create literal types

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var combine = function (input1, input2) {
+var combine = function (input1, input2, numConvert) {
     var result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
@@ -6,7 +6,14 @@ var combine = function (input1, input2) {
     else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+    if (numConvert === 'as-number') {
+        return +result; // To convert result as number
+    }
+    else {
+        return result.toString();
+    }
 };
-var combineAges = combine('Max', 45);
+var combineAges = combine('Max', 45, 'as-text');
+var combineNumber = combine('99', '32', 'as-number');
 console.log(combineAges);
+console.log(combineNumber);
--- script.ts
+++ script.ts
@@ -1,11 +1,22 @@
-const combine = function(input1: number | string, input2: number | string) {
+const combine = function(
+    input1: number | string, 
+    input2: number | string,
+    numConvert: 'as-number' | 'as-text',
+    ) {
     let result;
     if (typeof input1 === 'number' && typeof input2 === 'number') {
         result = input1 + input2;
     } else {
         result = input1.toString() + input2.toString();
     }
-    return result;
+    if(numConvert === 'as-number') {
+        return +result; // To convert result as number
+    } else {
+        return result.toString();
+    }
 }
-const combineAges = combine('Max', 45);
+const combineAges = combine('Max', 45, 'as-text');
+const combineNumber = combine('99', '32', 'as-number')
 console.log(combineAges);
+console.log(combineNumber);
+
