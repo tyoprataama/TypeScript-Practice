13b6df4a41b76920628bf0ea9a2b6b4efa88175c
tyoprataama <tyoprataama004@gmail.com>
2022-12-15 12:18:58 +0700

    create void types

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-var combines = function (input1, input2, numConvert) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || numConvert === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if(numConvert === 'as-number') {
-    //     return +result; // To convert result as number
-    // } else {
-    //     return result.toString();
-    // }
-};
-var combinesAges = combines('Max', 45, 'as-text');
-var combinesNumber = combines('99', '32', 'as-number');
-console.log(combinesAges);
-console.log(combinesNumber);
+function add(n1, n2) {
+    return n1 + n2;
+}
+function printRes(num) {
+    console.log('Result:' + num);
+}
+printRes(add(3, 5));
--- script.ts
+++ script.ts
@@ -1,23 +1,7 @@
-const combines = function(
-    input1: number | string, 
-    input2: number | string,
-    numConvert: 'as-number' | 'as-text',
-    ) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || numConvert === 'as-number') {
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if(numConvert === 'as-number') {
-    //     return +result; // To convert result as number
-    // } else {
-    //     return result.toString();
-    // }
+function add(n1: number, n2: number) {
+    return n1 + n2;
 }
-const combinesAges = combines('Max', 45, 'as-text');
-const combinesNumber = combines('99', '32', 'as-number')
-console.log(combinesAges);
-console.log(combinesNumber);
-
+function printRes(num: number):void {
+    console.log('Result:' + num);
+}
+printRes(add(3,5));
