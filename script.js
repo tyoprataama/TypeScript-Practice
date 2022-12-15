1cac616ea4ab7df5fb8e7fc92eebfadf1834bb0f
tyoprataama <tyoprataama004@gmail.com>
2022-12-15 19:35:10 +0700

    create function types and callback

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ function printRes(num) {
     console.log('Result:' + num);
 }
 printRes(add(3, 5));
+var combinesVal;
+combinesVal = add;
+function cbFunc(n1, n2, cb) {
+    var res = n1 + n2;
+    cb(res);
+}
+cbFunc(23, 22, function (result) {
+    console.log(result);
+});
--- script.ts
+++ script.ts
@@ -8,3 +8,12 @@ printRes(add(3,5));
 
 let combinesVal: (a: number, b: number) => number;
 combinesVal = add;
+
+function cbFunc(n1: number, n2: number, cb:(num:number) => void) {
+    const res = n1 + n2;
+    cb(res);
+}
+
+cbFunc(23, 22, (result) => {
+    console.log(result);
+})
