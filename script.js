0a8d9405791594ff59a6d820b2086e2d3058379f
tyoprataama <tyoprataama004@gmail.com>
2022-12-17 19:35:46 +0700

    set up files

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-//  unknown types
-var userInput; // the difference between any and unknown is we can put anything in unknown
-var userName;
-userInput = 5;
-userInput = 'Max';
-// userName = userInput (it will show error bcs unknown don't know what exactly userInput is)
-if (typeof userInput === 'string') {
-    userName = userInput; // we can figure it out through if statement
-}
-//  never types is newer types from void
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError('An Error occured in line 30!', 500);
+console.log('compiler');
--- script.ts
+++ script.ts
@@ -1,16 +1,1 @@
-//  unknown types
-let userInput: unknown; // the difference between any and unknown is we can put anything in unknown
-let userName: string;
-
-userInput = 5;
-userInput = 'Max';
-// userName = userInput (it will show error bcs unknown don't know what exactly userInput is)
-if(typeof userInput === 'string') {
-    userName = userInput; // we can figure it out through if statement
-}
-
-//  never types is newer types from void
-function generateError(message: string, code: number):never {
-    throw{ message: message, errorCode: code }
-}
-generateError('An Error occured in line 30!', 500);
+console.log('compiler');
