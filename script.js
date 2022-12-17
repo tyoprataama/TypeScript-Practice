c8cbf3d0fc48e479737d02945c76b803663b7570
tyoprataama <tyoprataama004@gmail.com>
2022-12-17 19:13:08 +0700

    create never types

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//  unknown types
 var userInput; // the difference between any and unknown is we can put anything in unknown
 var userName;
 userInput = 5;
@@ -6,3 +7,8 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
     userName = userInput; // we can figure it out through if statement
 }
+//  never types is newer types from void
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An Error occured in line 30!', 500);
--- script.ts
+++ script.ts
@@ -1,3 +1,4 @@
+//  unknown types
 let userInput: unknown; // the difference between any and unknown is we can put anything in unknown
 let userName: string;
 
@@ -7,3 +8,9 @@ userInput = 'Max';
 if(typeof userInput === 'string') {
     userName = userInput; // we can figure it out through if statement
 }
+
+//  never types is newer types from void
+function generateError(message: string, code: number):never {
+    throw{ message: message, errorCode: code }
+}
+generateError('An Error occured in line 30!', 500);
