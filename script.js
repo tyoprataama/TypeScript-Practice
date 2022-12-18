5ecff6d0bf0bb9dceef2fa3113ec7f55d867c87d
tyoprataama <tyoprataama004@gmail.com>
2022-12-18 18:58:23 +0700

    initialize tsconfig

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('compiler');
+"use strict";
+let age;
+age = 30;
+console.log(age);
+let button = document.querySelector('button');
+button === null || button === void 0 ? void 0 : button.addEventListener('click', () => {
+    console.log('Button clicked!');
+});
--- script.ts
+++ script.ts
@@ -1,1 +1,8 @@
-console.log('compiler');
+let age: number;
+age = 30;
+console.log(age);
+
+let button = document.querySelector('button');
+button?.addEventListener('click', ()=> {
+    console.log('Button clicked!');
+})
