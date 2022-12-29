07cd93efa636abe3cccce610a131ad1086e515c9
tyoprataama <tyoprataama004@gmail.com>
2022-12-29 09:36:03 +0700

    create spread operators

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ let button = document.querySelector('button');
 button === null || button === void 0 ? void 0 : button.addEventListener('click', () => {
     console.log('Button clicked!');
 });
+//  Spread operator
+const hobbies = ['Watching', 'Gaming'];
+const activeHobbies = ['Coding'];
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
--- script.ts
+++ script.ts
@@ -6,3 +6,11 @@ let button = document.querySelector('button');
 button?.addEventListener('click', ()=> {
     console.log('Button clicked!');
 })
+
+//  Spread operator
+const hobbies = ['Watching', 'Gaming'];
+const activeHobbies = ['Coding'];
+
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+
