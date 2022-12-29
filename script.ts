679891fabf119113a1889dd1743b6eaf832041f5
tyoprataama <tyoprataama004@gmail.com>
2022-12-29 09:47:45 +0700

    create rest parameters

--- script.ts
+++ script.ts
@@ -14,3 +14,20 @@ const activeHobbies = ['Coding'];
 activeHobbies.push(...hobbies);
 console.log(activeHobbies);
 
+//  Rest parameters
+const Add = (...numbers: number[]) => { //  get unlimited params
+    return numbers.reduce((curNum, curVal) => {
+        return curNum + curVal;
+    }, 0)
+}
+const restParam = Add(2,4,6,75,3,2);
+console.log(restParam);
+
+const Adding = (...numbers: [number, number, number]) => { //  get limited params
+    return numbers.reduce((currNum, currVal) => {
+        return currNum + currVal;
+    }, 0)
+}
+const unrestParam = Adding(2,65,21)
+console.log(unrestParam);
+
