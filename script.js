e507945472134f995e927efc06bd98c0a6aa9d78
tyoprataama <tyoprataama004@gmail.com>
2023-01-20 09:08:31 +0700

    shorthand initialization

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 "use strict";
 //  Define a class
 class Department {
-    constructor(n) {
-        this.name = n;
+    constructor(n, id) {
+        this.n = n;
+        this.id = id;
+        // public name: string;
+        this.student = []; //  Prevent edit or push outside classes
+        // this.name = n;
     }
     describe() {
-        console.log('Department: ' + this.name);
+        console.log(`Department(${this.id}): ${this.n}`);
+    }
+    addStudent(students) {
+        this.student.push(students);
+    }
+    describeStudent() {
+        console.log(this.student.length);
+        console.log(this.student);
     }
 }
-const newDept = new Department('Accounting');
+const newDept = new Department('Accounting', 'S1');
 console.log(newDept.describe());
-const newDeptCopy = { name: 'Economy', describe: newDept.describe }; //  Must have name property bcs it refers to Department class
-newDeptCopy.describe();
+newDept.addStudent('');
+newDept.addStudent('');
+// newDept.student[2] = '';    this not allowed when private method comes out
+newDept.describeStudent();
+// const newDeptCopy = { name: 'Economy', describe: newDept.describe } //  Must have name property bcs it refers to Department class
+// newDeptCopy.describe();
--- script.ts
+++ script.ts
@@ -1,18 +1,28 @@
 //  Define a class
 class Department {
-    name: string;
-    constructor(n: string) {
-        this.name = n;
+    // public name: string;
+    private student: string[] = []; //  Prevent edit or push outside classes
+    constructor(private n: string, private id: string) {    //  Simplified the code
+        // this.name = n;
     }
     describe(this: Department) { // Adding this parameters to avoid undefined
-        console.log('Department: ' + this.name);
-        
+        console.log(`Department(${this.id}): ${this.n}`);
+    }
+    addStudent(students: string) {
+        this.student.push(students);
+    }
+    describeStudent() {
+        console.log(this.student.length);
+        console.log(this.student);
     }
 }
 
-const newDept = new Department('Accounting');
+const newDept = new Department('Accounting', 'S1');
 console.log(newDept.describe());
-
-const newDeptCopy = { name: 'Economy', describe: newDept.describe } //  Must have name property bcs it refers to Department class
-newDeptCopy.describe();
+newDept.addStudent('')
+newDept.addStudent('')
+// newDept.student[2] = '';    this not allowed when private method comes out
+newDept.describeStudent();
+// const newDeptCopy = { name: 'Economy', describe: newDept.describe } //  Must have name property bcs it refers to Department class
+// newDeptCopy.describe();
 
